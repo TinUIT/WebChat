7ae5f34f9e9b305c3f86ec46a359543202d4999e
TinUIT <20522017@gm.uit.edu.vn>
2022-12-02 08:02:52 +0700

    migrate

--- authentication/models.py
+++ authentication/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile (models.Model):
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    email = models.ForeignKey(User, on_delete=models.CASCADE,primary_key=True)
+    year = models.IntegerField()
