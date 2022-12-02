f13fd1997ad7c1c69693901c3a888c9a2e574839
TinUIT <20522017@gm.uit.edu.vn>
2022-12-02 08:12:02 +0700

    t

--- authentication/models.py
+++ authentication/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile (models.Model):
-    email = models.ForeignKey(User, on_delete=models.CASCADE,primary_key=True)
+    GENDER = [
+        ("M","male"),
+        ("F","female")
+    ]
+    email = models.ForeignKey(User, on_delete=models.CASCADE)
     year = models.IntegerField()
+    gender = models.CharField(default="",max_length=6,choices=GENDER)
