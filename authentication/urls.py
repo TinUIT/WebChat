4680bfad50e5f75abb42368d9a42854c6e845420
TinUIT <20522017@gm.uit.edu.vn>
2022-12-12 14:06:42 +0700

    changepassword

--- authentication/urls.py
+++ authentication/urls.py
@@ -27,25 +27,25 @@ urlpatterns = [
     ),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
-            #  template_name='password-reset/password_reset.html',
+             template_name='password-reset/password_reset.html',
             #  subject_template_name='password-reset/password_reset_subject.txt',
-            #  email_template_name='password-reset/password_reset_email.html',
+             email_template_name='password-reset/password_reset_email.html',
             #  success_url='/signin/'
          ),
          name='password_reset'),
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
-            #  template_name='password-reset/password_reset_done.html'
+             template_name='password-reset/password_reset_done.html'
          ),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-            #  template_name='password-reset/password_reset_confirm.html'
+             template_name='password-reset/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(
-            #  template_name='password-reset/password_reset_complete.html'
+             template_name='password-reset/password_reset_complete.html'
          ),
          name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
